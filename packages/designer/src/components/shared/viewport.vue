d1d6be4859829564557733504b2e3244668aca45
jiangxulai <1123834112@qq.com>
2025-03-21 13:53:45 +0800

    增加自定义屏幕尺寸

--- packages/designer/src/components/shared/viewport.vue
+++ packages/designer/src/components/shared/viewport.vue
@@ -6,24 +6,29 @@
 <script lang="ts" setup>
   import { computed } from 'vue';
   import { MOBILE_SIZE, PAD_SIZE } from '../../constants';
+
+  
   export interface Props {
-    mode?: 'pc' | 'pad' | 'mobile';
+    mode?: 'pc' | 'pad' | 'mobile' | 'custom';
     width?: number;
     height?: number;
+    customSize?: { width : number, height : number }
   }
 
   const props = withDefaults(defineProps<Props>(), {
-    mode: 'pc'
+    mode: 'pc',
+    customSize: ()=>({ width : 1920, height : 1080})
   });
 
   const classes = computed(() => {
     return [`is-${props.mode}`];
   });
 
+  const size = computed(()=>props.mode === 'mobile' ? MOBILE_SIZE : props.mode === 'pad' ? PAD_SIZE : props.customSize)
+
   const scale = computed(() => {
     if (props.mode === 'pc') return 1;
-    const size = props.mode === 'mobile' ? MOBILE_SIZE : PAD_SIZE;
-    const { width, height } = size;
+    const { width, height } = size.value;
     const widthScale = width > 0 ? (props.width || 0) / width : 1;
     const heightScale = height > 0 ? (props.height || 0) / height : 1;
     return Math.min(widthScale, heightScale);
@@ -31,8 +36,7 @@
 
   const styles = computed(() => {
     if (props.mode === 'pc') return {};
-    const size = props.mode === 'mobile' ? MOBILE_SIZE : PAD_SIZE;
-    const { width, height } = size;
+    const { width, height } = size.value;
     return {
       width: `${width}px`,
       height: `${height}px`,
