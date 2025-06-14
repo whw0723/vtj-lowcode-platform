ab1c415202ee7a35013963f924226b0839b82f39
“chenhuachun” <samchen08@163.com>
2025-06-14 14:44:09 +0800

    chore: 🚀 更新依赖

--- lcdp/src/designer.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from '@/views/designer/app.vue';
-import router from '@/router/designer';
-import '@/style/index.scss';
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
--- lcdp/src/yx.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import App from '@/views/Volunteer.vue';
-import '@/style/index.scss';
-const app = createApp(App);
-app.mount('#app');
