93082530ac9711a2c979c6751bd2e084b41265e4
“chenhuachun” <samchen08@163.com>
2025-06-30 09:05:00 +0800

    docs: ✏️  news

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -1,25 +1,31 @@
 export default [
-  { text: '文档', link: '/guide/' },
+  { text: '指引', link: '/guide/' },
   { text: '维基', link: '/wiki/' },
-  { text: '工具库', link: '/utils/' },
-  { text: 'UI库', link: '/ui/' },
-  // { text: '图表库', link: '/icons/' },
-  { text: 'API', link: '/typedoc/modules.html' },
   { text: '🔥 高级服务', link: '/service/' },
+  { text: '动态', link: '/news/' },
   {
-    text: '相关链接',
+    text: '文档',
     items: [
-      { text: 'Vue.js', link: 'https://cn.vuejs.org/api/' },
-      { text: 'Vue Router', link: 'https://router.vuejs.org/zh/api/' },
-      { text: 'Element Plus', link: 'https://element-plus.org/zh-CN/' },
-      {
-        text: 'VueUse',
-        link: 'https://vueuse.org/functions.html'
-      },
-      {
-        text: 'Ant Design Vue',
-        link: 'https://www.antdv.com/components/overview-cn/'
-      }
+      { text: '工具库', link: '/utils/' },
+      { text: 'UI库', link: '/ui/' },
+      // { text: '图表库', link: '/icons/' },
+      { text: 'API', link: '/typedoc/modules.html' }
     ]
   }
+  // {
+  //   text: '相关链接',
+  //   items: [
+  //     { text: 'Vue.js', link: 'https://cn.vuejs.org/api/' },
+  //     { text: 'Vue Router', link: 'https://router.vuejs.org/zh/api/' },
+  //     { text: 'Element Plus', link: 'https://element-plus.org/zh-CN/' },
+  //     {
+  //       text: 'VueUse',
+  //       link: 'https://vueuse.org/functions.html'
+  //     },
+  //     {
+  //       text: 'Ant Design Vue',
+  //       link: 'https://www.antdv.com/components/overview-cn/'
+  //     }
+  //   ]
+  // }
 ];
